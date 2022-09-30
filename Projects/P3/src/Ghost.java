05be1c547d6bcc0d50a4cad993e29fd3dfd815fa
Sahil Sinha <ssinha28@umd.edu>
2022-09-30 11:43:02 -0400

    Implemented ghost functions

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -16,7 +16,17 @@ public class Ghost {
   }
 
   public boolean move() {
-    return false;
+    ArrayList<Location> moves = get_valid_moves();
+    int idx = 0;
+    if (moves == null) {
+      return false;
+    }
+    if (!this.myMap.move(this.myName, moves.get(idx), Map.Type.GHOST))
+      return false;
+    idx = (int) (Math.random() * moves.size());
+    this.shift = this.myLoc.unshift(moves.get(idx));
+    this.myLoc = moves.get(idx);
+    return true;
   }
 
   public boolean is_pacman_in_range() {
--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -1,7 +1,19 @@
 import java.io.*;
 import junit.framework.*;
+import java.awt.Color;
 
 public class TestGhostMove extends TestCase {
 
-  public void testGhostMove() throws FileNotFoundException {}
+  public void testGhostMove() throws FileNotFoundException{
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(1,1), "binky", Color.red);
+    assertTrue(ghost.move());
+
+    Location loc1 = new Location(1,2);
+    Location loc2 = new Location(2,1);
+    Location loc3 = new Location(2,2);
+
+    System.out.println(ghost.myLoc.x + " " + ghost.myLoc.y);
+    assertTrue(ghost.myLoc.equals(loc1) || ghost.myLoc.equals(loc2) || ghost.myLoc.equals(loc3));
+  }
 }
