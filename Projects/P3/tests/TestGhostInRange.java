96340d40ee52fe9459837482e4faced1b612ee88
Suraj Narang <snarang@umd.edu>
2022-10-03 14:11:23 -0400

    implemented isGhostInRange and implemented testing

--- Projects/P3/tests/TestGhostInRange.java
+++ Projects/P3/tests/TestGhostInRange.java
@@ -1,9 +1,14 @@
 import java.io.*;
 import junit.framework.*;
+import java.awt.Color;
 
 public class TestGhostInRange extends TestCase {
 
   public void testGhostInRange() throws FileNotFoundException {
-    return null;
+    NoFrame frame = new NoFrame(); 
+    Ghost ghost = frame.addGhost(new Location(5, 5), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(6, 5));
+
+    assertTrue(pacman.is_ghost_in_range() == true);
   }
 }
