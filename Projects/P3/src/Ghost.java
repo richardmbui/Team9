f08b7c4188253d58c1bb0f9985fd9e94091ff1a3
Kishore Nagarajan <kishorenaga4@gmail.com>
2022-10-21 14:07:30 -0400

    Fix validMoves

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -58,30 +58,30 @@ public class Ghost {
   }
 
   public boolean is_pacman_in_range() {
-    int x_pm = myLoc.y;
-    int y_pm = myLoc.x;
+    int x_pm = myLoc.x;
+    int y_pm = myLoc.y;
 
     if (myMap.getLoc(new Location(x_pm + 1, y_pm)).contains(Map.Type.PACMAN)) {
-      return false;
+      return true;
     }
 
     if (myMap.getLoc(new Location(x_pm - 1, y_pm)).contains(Map.Type.PACMAN)) {
-      return false;
+      return true;
     }
 
     if (myMap.getLoc(new Location(x_pm, y_pm + 1)).contains(Map.Type.PACMAN)) {
-      return false;
+      return true;
     }
     if (myMap.getLoc(new Location(x_pm, y_pm - 1)).contains(Map.Type.PACMAN)) {
-      return false;
+      return true;
     }
 
     if (myMap.getLoc(new Location(x_pm + 1, y_pm - 1)).contains(Map.Type.PACMAN)) {
-      return false;
+      return true;
     }
 
     if (myMap.getLoc(new Location(x_pm - 1, y_pm + 1)).contains(Map.Type.PACMAN)) {
-      return false;
+      return true;
     }
     return false;
   }
